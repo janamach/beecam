886efd7064be27096681ab63ccc1c44726d9d03f
Jana Mach <39128466+janamach@users.noreply.github.com>
2022-05-09 10:38:41 +0200

    Update recorder.sh

--- recorder.sh
+++ recorder.sh
@@ -11,8 +11,7 @@ while true; do
       --ok-label Quit \
       --extra-button 30 \
       --extra-button 10 \
-      --extra-button 5 \
-      --extra-button 2 \
+      --extra-button 1 \
       --extra-button "Power off" \
        )
 
@@ -46,6 +45,7 @@ VLENGTH=$((ans * 60000))
 raspivid -t ${VLENGTH} -b 1500000 -sa -100 -fps 30 -w 1920 -h 1080 -p 10,10,160,90 -o ~/Videos/bees_${FNUMBER}.h264 & \
     ( for i in `seq 1 100`; do echo $i ; echo "#Recording $ans minutes. Progress: $i %";  sleep $((ans * 60 / 100)); done ) \
     | yad --progress --center --borders=20 --button 'Cancel video recording:killall raspivid & killalll yad' && \
+    killalll yad && \
 
 MP4Box -add ~/Videos/bees_${FNUMBER}.h264:fps=29.997 ~/Videos/bees_${FNUMBER}.mp4 && \
     yad --info --text "Video converted to mp4" --title="Info" --button="OK" --borders=20 --center
