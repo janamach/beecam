482430d557cdcb88c1b8fdced35c7b9ebe3a8aae
Jana Mach <39128466+janamach@users.noreply.github.com>
2022-05-11 16:16:28 +0200

    Update recorder.sh

--- recorder.sh
+++ recorder.sh
@@ -7,6 +7,15 @@
 # Do 'sudo nano /etc/apt/sources.list' and uncomment 'deb-src ...'
 # Do 'sudo nano /etc/apt/apt.conf.d/10defaultRelease' and replace 'stable' with 'stretch'
 
+# Edit for video settings
+
+fps=30
+video_width=1920
+video_height=1080
+
+# Edit for file numbering
+count_from=1000
+
 while true; do
   ans=$(zenity --info --title 'Record a video' \
       --text 'Choose video duration in minutes' \
@@ -35,7 +44,7 @@ if [ -f count ]; then
     echo "count file exists."
 else
     echo "count file does not exist. Creating ..."    
-    echo 0 > count
+    echo ${count_from} > count
 fi
 
 ### Add 1 to count
@@ -46,10 +55,15 @@ echo ${FNUMBER} > count
 VLENGTH=$((ans * 60000))
 # echo ${VLENGTH} > ans
 
-yad --timeout-indicator=top --posx=120 --posy=230 --timeout=$((ans * 60 + 5)) --borders=20 --text="Recording bees_${FNUMBER}.h264" --button 'Cancel video recording:killall raspivid & killall yad'  & \
-raspivid -t ${VLENGTH} -b 1500000 -sa -100 -fps 30 -w 1920 -h 1080 -p 48,0,400,225 -o ~/Videos/bees_${FNUMBER}.h264
+yad --timeout-indicator=top --posx=120 --posy=230 \
+    --timeout=$((ans * 60 + 5)) --borders=20 \
+    --text="Recording bees_${FNUMBER}.h264" \
+    --button 'Cancel video recording:killall raspivid & killall yad'  & \
 
-MP4Box -add ~/Videos/bees_${FNUMBER}.h264:fps=29.997 ~/Videos/bees_${FNUMBER}.mp4 && \
-    yad --info --text "Video converted to bees_${FNUMBER}.mp4" --title="Info" --button="OK" --borders=20 --center
+raspivid -t ${VLENGTH} -b 1500000 -sa -100 -fps ${fps} -w ${video_width} -h ${video_height} -p 48,0,400,225 -o ~/Videos/bees_${FNUMBER}.h264
+
+MP4Box -add ~/Videos/bees_${FNUMBER}.h264:fps=${fps} ~/Videos/bees_${FNUMBER}.mp4 && \
+
+yad --info --text "Video converted to bees_${FNUMBER}.mp4" --title="Info" --button="OK" --borders=20 --center
 
 done
