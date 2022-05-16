a9963848fec830f97ee026645b05e210d0cc9c2b
Jana Mach <39128466+janamach@users.noreply.github.com>
2022-05-16 13:54:45 +0200

    Create rpi_sd.sh

---
+++ sd_card_tool/rpi_sd.sh
@@ -0,0 +1,5 @@
+yad --into --center --borders=20 \
+    --title='Raspberry Pi SD Card' \
+    --text='<big>Insert the Raspberry Pi SD card and selection action:</big>' \
+    --button="Open Videos":"nautilus /media/jana/rootfs/home/pi/Videos/" \
+    --button "Unmount SD Card":"umount /media/jana/rootfs /media/jana/boot" && "yad --info --text='Done!'"
