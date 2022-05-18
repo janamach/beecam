12fe857b56388acf5270fbe2d23c040b6c08209c
Jana Mach <39128466+janamach@users.noreply.github.com>
2022-05-18 13:04:17 +0200

    Update first_boot.sh

--- first_boot.sh
+++ first_boot.sh
@@ -1,13 +1,10 @@
-# Enable camera in raspi-config
-
 sudo apt update
 sudo apt install -y gpac yad
 git clone https://github.com/janamach/beecam.git
-echo "Enable camera and expand file system
+echo bash ~/beecam/recorder.sh > .profile
+echo "Expand file system"
 sudo raspi-config
 
 ### Do 'sudo nano /etc/apt/sources.list' and uncomment 'deb-src ...'
 ### Do 'sudo nano /etc/apt/apt.conf.d/10defaultRelease'
 ### and replace 'stable' with 'stretch'
-
-echo bash ~/beecam/recorder.sh > .profile
