1a13cde0f6160b57a93743bb2be5da963d7f6fa9
Jana Mach <39128466+janamach@users.noreply.github.com>
2022-05-18 13:49:14 +0200

    Update recorder.sh

--- recorder.sh
+++ recorder.sh
@@ -85,7 +85,7 @@ echo ${FNUMBER} > count
 VLENGTH=$((ans * 60000))
 # echo ${VLENGTH} > ans
 
-yad --timeout-indicator=top --posx=40 --posy=225 \
+yad --timeout-indicator=top --posx=80 --posy=225 \
     --timeout=$((ans * 60 + 5)) \
     --text="Recording ${VID_DIR}/bees_${FNUMBER}.h264" \
     --button 'Cancel video recording:killall raspivid & killall yad'  & \
