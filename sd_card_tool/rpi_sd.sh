4d46a7e592ac930cf4e861e15193bcf5d87a9d1e
Jana Mach <39128466+janamach@users.noreply.github.com>
2022-05-23 17:35:26 +0200

    Update rpi_sd.sh

--- sd_card_tool/rpi_sd.sh
+++ sd_card_tool/rpi_sd.sh
@@ -3,3 +3,7 @@ yad --into --center --borders=20 \
     --text='<big>Insert the Raspberry Pi SD card and selection action:</big>' \
     --button="Open Videos":"nautilus /media/jana/rootfs/home/pi/Videos/" \
     --button "Unmount SD Card":"umount /media/jana/rootfs /media/jana/boot" && "yad --info --text='Done!'"
+    --button ">> Write SD card <<":"yad --info --text 'Insert an SD Card and press Write' \
+            --button='Back' \
+            --button='Write SD'='gnome-terminal -- htop'
+            "
