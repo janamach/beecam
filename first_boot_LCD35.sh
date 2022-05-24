4ddc6f6565961617766d18d4663f8660c7fad32e
Jana Mach <39128466+janamach@users.noreply.github.com>
2022-05-24 13:36:23 +0200

    Update first_boot_LCD35.sh

--- first_boot_LCD35.sh
+++ first_boot_LCD35.sh
@@ -8,7 +8,7 @@ sudo apt dist-upgrade -y
 cd beecam
 git pull
 
-cd LCD-show
+cd ~/LCD-show
 bash LCD35-show 180 & echo '\
    sudo sed -i "s/start_x=0/start_x=1/g" /boot/config.txt && \
    mv .profile_original .profile && sudo reboot' \
