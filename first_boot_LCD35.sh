9436a5c4bf28c368f0656d852d30799948f1bb41
Jana Mach <39128466+janamach@users.noreply.github.com>
2022-05-24 11:05:34 +0200

    Create first_boot_LCD35.sh

---
+++ first_boot_LCD35.sh
@@ -0,0 +1,16 @@
+# First:
+# cp .profile .profile_original
+# echo "bash ~/first_boot_LCD35.sh" > .profile
+
+sudo apt update
+sudo apt dist-upgrade
+
+cd beecam
+git pull
+
+cd LCD-show
+bash LCD35-show 180 & echo '\
+   sudo sed -i "s/start_x=0/start_x=1/g" /boot/config.txt && \
+   mv .profile_original .profile && sudo reboot' \
+   > ~/.profile
+sudo reboot
